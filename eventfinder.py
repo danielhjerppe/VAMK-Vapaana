9717be23f4e92b89a4085a00c08b02bf53eb0a15
danielhjerppe <daniel.hjerppe@gmail.com>
2023-09-17 13:54:37 +0300

    First version

--- eventfinder.py
+++ eventfinder.py
@@ -7,20 +7,16 @@ import urllib3
 
 def add_rooms(session):
     url = "https://lukkarit.vamk.fi/rest/basket/0/location"
-    with open("Wolffintie-rooms.json", "r") as file:
-        data = json.load(file)
-    for i in range(len(data)):  # TESTING, restricting amount of rooms to add. Replace with len(data)
-        data[i]["adding"] = True
-        print(f"Adding room {i} of {len(data)}. Payload = {data[i]}")
-        session.request("POST", url, json=data[i], headers=session.headers, verify=False)
+    with open("Wolffintie-rooms.json", "r") as rooms_file:
+        room_data = json.load(rooms_file)
+    for i in range(len(room_data)):  # Restrict amount of rooms for testing
+        room_data[i]["adding"] = True
+        print(f"Adding room {i+1} of {len(room_data)}. Payload = {room_data[i]}")
+        session.request("POST", url, json=room_data[i], headers=session.headers, verify=False)
         time.sleep(0.1)
 
 
-def read_events(session):
-    today_date = datetime.now().strftime("%Y-%m-%d")
-    tomorrow_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
-    today_date = "2023-09-18"  # TESTING
-    tomorrow_date = "2023-09-19"  # TESTING
+def read_events(session, today_date, tomorrow_date):
     url = "https://lukkarit.vamk.fi/rest/basket/0/events"
     payload = {
         "dateFrom": f"{today_date}",
@@ -29,12 +25,42 @@ def read_events(session):
     }
     response = session.request("POST", url, json=payload, headers=session.headers, verify=False)
     data = json.loads(response.text)
-    with open(f"{today_date}-events.json", "w") as json_file:
+    with open(f"{today_date}_events.json", "w") as json_file:
         json.dump(data, json_file, indent=5)
 
 
+def clean_events(today_date):
+    with open(f"{today_date}_events.json", "r") as event_file:
+        event_data = json.load(event_file)
+    with open("Wolffintie-rooms.json", "r") as room_file:
+        room_data = json.load(room_file)
+
+    room_reservations = []
+    for room_item in room_data:
+        classroom = room_item["class"]
+        reservations_by_room = {
+            "class": classroom,
+            "start": [],
+            "end": []
+        }
+
+        for event_item in event_data:
+            # Check if the "location" key has a dictionary with "class" equal to {classroom}
+            if any(loc.get("class") == classroom for loc in event_item.get("location", [])):
+                reservations_by_room["start"].append(event_item["start_date"])
+                reservations_by_room["end"].append(event_item["end_date"])
+        room_reservations.append(reservations_by_room)
+
+    with open(f"{today_date}_clean_events.json", "w") as json_file:
+        json.dump(room_reservations, json_file, indent=5)
+
 
 def main():
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    tomorrow_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+    today_date = "2023-09-18"  # TESTING
+    tomorrow_date = "2023-09-19"  # TESTING
+
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     url = "https://lukkarit.vamk.fi/rest/user"
     custom_headers = {
@@ -50,7 +76,8 @@ def main():
     print(f"Cooke: {csrf_token}\n")
 
     add_rooms(session)
-    read_events(session)
+    read_events(session, today_date, tomorrow_date)
+    clean_events(today_date)
 
 
 if __name__ == "__main__":
