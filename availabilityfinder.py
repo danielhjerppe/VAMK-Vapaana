27a57ca9b23a5ff62aed9a77dc010d91ff68f3f6
danielhjerppe <daniel.hjerppe@gmail.com>
2023-09-20 00:31:18 +0300

    Added floor and wing

--- availabilityfinder.py
+++ availabilityfinder.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 def check_room_availability(time_now, today_date):
@@ -9,9 +9,9 @@ def check_room_availability(time_now, today_date):
     availability_now = []
 
     for event in event_data:
-        avail_dict = {"class": event["class"], "available": "", "number_of_events": len(event["start"])}
         available = True
-        avail_dict["available"] = available
+        avail_dict = {"class": event["class"], "available": available, "number_of_events": len(event["start"]),
+                      "floor": which_floor(event), "wing": which_wing(event)}
         if len(event["start"]) > 0:
             for i in range(len(event["start"])):
                 starttime = datetime.strptime(event["start"][i], "%Y-%m-%d %H:%M")
@@ -26,6 +26,8 @@ def check_room_availability(time_now, today_date):
 
 def print_to_terminal(rooms_available):
     for room_availability in rooms_available:
+        print(f"{room_availability['floor']}", end="")
+        print(f"{room_availability['wing']}", end="\t")
         print(f"Room: {room_availability['class']} ", end="\t")
         print(f"{room_availability['number_of_events']} event(s)", end="\t")
         if room_availability['available']:
@@ -35,18 +37,39 @@ def print_to_terminal(rooms_available):
         print(f"Available: {room_availability['available']}", end="")
         print("\033[0m")
 
+
+def which_floor(event):
+    if event["class"][1] == "1":
+        return 1
+    elif event["class"][1] == "2":
+        return 2
+    elif event["class"][1] == "3":
+        return 3
+    else:
+        return ""
+
+
+def which_wing(event):
+    if event["class"][0] == "A":
+        return "A"
+    elif event["class"][0] == "B":
+        return "B"
+    elif event["class"][0] == "C":
+        return "C"
+    else:
+        return ""
+
+
 def main():
     time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
-    #time_now = "2023-09-18 13:00"
+    # time_now = "2023-09-18 13:00"
     today_date = datetime.now().strftime("%Y-%m-%d")
-    #today_date = "2023-09-18"  # TESTING
+    # today_date = "2023-09-18"  # TESTING
 
     print(f"{time_now = }\n")
 
     rooms_available = check_room_availability(time_now, today_date)
     print_to_terminal(rooms_available)
-
-
 
 
 if __name__ == "__main__":
